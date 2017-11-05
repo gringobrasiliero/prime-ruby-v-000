8e82b07b99b6dc74c06c2523286276fdd67f1c05
Nolan Byrnes <nolanehs@gmail.com>
2017-11-05 14:33:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+
+  if n <= 1 
+    return false
+  end
+  elsif n <= 3
+    return true
+  end
+  elsif n%2 = 0 || n%3 = 0
+    return false
+  end  
+  i < 5
+  
+    
+  end  
+end
