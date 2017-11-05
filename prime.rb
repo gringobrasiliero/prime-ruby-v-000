2a3c78c35c43cf01432b1666c662c015f5288eaa
Nolan Byrnes <nolanehs@gmail.com>
2017-11-05 15:39:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,4 @@
 # Add  code here!
 def prime?(num)
-
-  if num <= 1
-    return false
-  end
-elsif num <= 3
-    return true
-  end
-elsif num%2 = 0 || num%3 = 0
-    return false
-  end
-  i <= 5
-
-while i * i <= num do
-  if num%i = 0 || num%(i+2) = 0
-    return false
-end
-  elsif i <= i + 6
-    return true
-  end
+return false if num <= 1
+return true if num <= 3
